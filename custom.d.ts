26e0c6d6e4f3bf5ac66eaeda8a2273c8a1afff12
M. Zole <zole@zole.org>
2022-04-26 21:26:58 -0700

    Add types for imported assets

---
+++ custom.d.ts
@@ -0,0 +1,14 @@
+declare module '*.svg' {
+    const content: any
+    export default content
+}
+
+declare module '*.gltf' {
+    const content: string
+    export default content
+}
+
+declare module '*.glb' {
+    const content: string
+    export default content
+}
