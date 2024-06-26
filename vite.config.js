ddb2d1b70f343750ff1c85a924861fe76a0f36f6
Alex Michaud <alex@bucketosoftware.com>
2024-06-25 17:55:58 -0700

    chore: Document the purpose of each vite plugin

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,26 @@
 import day from 'dayjs'
 import { defineConfig } from 'vite'
 
-// required by wasm plugin
 import react from '@vitejs/plugin-react'
 import glsl from 'vite-plugin-glsl'
 import mkcert from 'vite-plugin-mkcert'
 import topLevelAwait from 'vite-plugin-top-level-await'
 import wasm from 'vite-plugin-wasm'
 
-
 export default defineConfig((env) => ({
-    plugins: [mkcert(), topLevelAwait(), wasm(), react(), glsl()],
     assetsInclude: ['**/*.gltf', '**/*.glb'],
+    plugins: [
+        // serve over https, enabling various browser APIs
+        mkcert(),
+        // required by wasm plugin
+        topLevelAwait(),
+        // import wasm files
+        wasm(),
+        // support react-based interfaces
+        react(),
+        // import GLSL shaders
+        glsl(),
+    ],
 
     define: {
         IS_PRODUCTION: env.mode === 'production',
